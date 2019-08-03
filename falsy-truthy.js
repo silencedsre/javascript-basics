202f723379080dc6f25f22e18dcc7ee7db3a4a98
silenced sre <skr3sna@gmail.com>
2019-08-03 16:53:38 +0545

    falsy truthy

---
+++ falsy-truthy.js
@@ -0,0 +1,23 @@
+/*
+falsy values
+
+false
+null
+undefined
+0
+NaN
+''
+""
+
+
+Truthy values (everything else)
+
+true
+{}
+[]
+'non-empty-string'
+new RegExp() --new instance of an object
+10
+Infinity
+-Infinity
+ */
