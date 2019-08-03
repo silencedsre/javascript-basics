c64bf512454c33804d13c3a6b3068dd680ae827b
silenced sre <skr3sna@gmail.com>
2019-08-03 16:57:08 +0545

    null

--- null.js
+++ null.js
@@ -2,3 +2,6 @@ let pattern = /xyz/;
 let value = 'This is just a test';
 let result = value.match(pattern);
 console.log(result);
+
+//interesting is type of result(null) is object
+console.log(typeof result);
