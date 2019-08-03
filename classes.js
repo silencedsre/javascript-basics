1e7919c0d7b2095e3a39014f9d1af28d705d9c74
silenced sre <skr3sna@gmail.com>
2019-08-03 16:27:46 +0545

    javascript classes

---
+++ classes.js
@@ -0,0 +1,29 @@
+//declaration
+class Car{
+    constructor(make,model,year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    print(){
+        console.log(`${this.make} ${this.model} ${this.year}`)
+    }
+
+}
+let myCar = new Car('bmw','745li',2010);
+myCar.print()
+
+//expression
+// let car = class{
+//
+// }
+
+class SportsCar extends Car{
+    revEngine(){
+        console.log('Vrrom goes the ' + this.model);
+    }
+
+}
+let mySportsCar = new SportsCar('dodge','viper',2011);
+mySportsCar.print();
+mySportsCar.revEngine()
